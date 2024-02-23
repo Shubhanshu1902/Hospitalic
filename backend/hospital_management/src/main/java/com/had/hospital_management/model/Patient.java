e6bbba16d4ac63be9d18a8a9c4801478bdd4f8e8
Arin <arinawasthi05@gmail.com>
2024-02-24 00:04:06 +0530

    added patient table

---
+++ backend/hospital_management/src/main/java/com/had/hospital_management/model/Patient.java
@@ -0,0 +1,27 @@
+package com.had.hospital_management.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Patient {
+    @jakarta.persistence.Id
+    private Long Id;
+    private String First_Name;
+    private String Last_Name;
+    private String Email;
+    private String Gender;
+    private Date DOB;
+    private String Address;
+    private String Password;
+}
