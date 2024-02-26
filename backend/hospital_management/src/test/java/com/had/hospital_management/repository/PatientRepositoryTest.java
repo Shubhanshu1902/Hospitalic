e1a6922adbbf6b7775ceaa4919af5d2d1cba5407
vatsal dhama <thevatsaldhama@gmail.com>
2024-02-26 22:30:38 +0530

    "added report table"

---
+++ backend/hospital_management/src/test/java/com/had/hospital_management/repository/PatientRepositoryTest.java
@@ -0,0 +1,18 @@
+package com.had.hospital_management.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class PatientRepositoryTest {
+    @Autowired
+    private PatientRepository patientRepository;
+
+    @Test
+    public void savePatient() {
+        Patient patient = Patient.builder()
+    }
+}
