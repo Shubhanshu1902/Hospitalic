40f16e31690207a19a6a0cc8ad114af0bfa51456
vaibhav-vt <77973068+vaibhav-vt@users.noreply.github.com>
2024-02-28 22:48:35 +0530

    Update Messages.js

--- frontend/src/chat/js/Messages.js
+++ frontend/src/chat/js/Messages.js
@@ -1,6 +1,17 @@
+import hasbulla from "../../icons/Untitled.jpeg"
+
 const Messages = () => {
     return (
-        <div className="messages">Messages</div>
+        <div className="messages">
+            <div className="messageInfo">
+                <img src={hasbulla} alt="hasbulla"/>
+                <span>just now</span>
+            </div>
+            <div className="messageContent">
+                <p>hello</p>
+            </div>
+
+        </div>
     );
 }
 
