c3ffc9549e5b031335a97f6632d22c0cb199df67
vatsal dhama <thevatsaldhama@gmail.com>
2024-02-28 13:16:45 +0530

    added lot of backend

---
+++ backend/hospital_management/src/main/java/com/had/hospital_management/controller/PatientController.java
@@ -0,0 +1,28 @@
+package com.had.hospital_management.controller;
+
+import com.had.hospital_management.model.Patient;
+import com.had.hospital_management.service.PatientService;
+import com.had.hospital_management.service.PatientServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class PatientController {
+
+    @Autowired
+    private PatientService patientService;
+
+    @PostMapping("/patients")
+    public Patient savePatient(@RequestBody Patient patient) {
+        System.out.println("reached");
+        return patientService.savePatient(patient);
+    }
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String helloWorld() {
+        System.out.println("hello reached");
+        return "Hello wordl";
+    }
+
+
+}
