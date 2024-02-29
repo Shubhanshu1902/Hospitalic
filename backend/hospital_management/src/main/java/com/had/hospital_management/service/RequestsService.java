5065f9683b811a99e9ac34568ef72ab206f2c578
Arin0508 <arinawasthi05@gmail.com>
2024-02-29 12:20:53 +0530

    added requests controller

---
+++ backend/hospital_management/src/main/java/com/had/hospital_management/service/RequestsService.java
@@ -0,0 +1,37 @@
+package com.had.hospital_management.service;
+
+import com.had.hospital_management.model.Requests;
+import com.had.hospital_management.repository.RequestsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class RequestsService {
+    @Autowired
+    private RequestsRepository requestsRepository;
+
+    public Requests save(Requests Requests)
+    {
+        return requestsRepository.save(Requests);
+    }
+
+    public List<Requests> findAll()
+    {
+        return requestsRepository.findAll();
+    }
+
+    public Requests getById(Long Id)
+    {
+        return  requestsRepository.findById(Id).orElse(null);
+    }
+
+    @Transactional
+    public void deleteById(Long id) {
+        Requests Requests = requestsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Requests with id " + id + " not found"));
+        requestsRepository.delete(Requests);
+    }
+}
