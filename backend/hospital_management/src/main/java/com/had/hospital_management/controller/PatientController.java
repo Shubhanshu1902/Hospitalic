6f0d82c51428aaf42b7b1e3c4abe71bd84cb7656
vatsal dhama <thevatsaldhama@gmail.com>
2024-02-29 12:03:25 +0530

    patient controller build

--- backend/hospital_management/src/main/java/com/had/hospital_management/controller/PatientController.java
+++ backend/hospital_management/src/main/java/com/had/hospital_management/controller/PatientController.java
@@ -2,26 +2,43 @@ package com.had.hospital_management.controller;
 
 import com.had.hospital_management.model.Patient;
 import com.had.hospital_management.service.PatientService;
-import com.had.hospital_management.service.PatientServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/patient")
 public class PatientController {
 
     @Autowired
     private PatientService patientService;
 
-    @PostMapping("/patients")
-    public Patient savePatient(@RequestBody Patient patient) {
+    @PostMapping("/save")
+    public Patient save(@RequestBody Patient patient) {
         System.out.println(patient);
         System.out.println("reached");
-        return patientService.savePatient(patient);
+        return patientService.save(patient);
 //        return "patientpencho";
     }
 
+    @GetMapping("/find_all")
+    public List<Patient> findAll(){
+        return patientService.findAll();
+    }
 
-    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    @GetMapping("/get_by_id/{id}")
+    public ResponseEntity<Patient> getById(@PathVariable("id") Long id){
+        Patient patient = patientService.findById(id);
+        if(patient != null) {
+            return ResponseEntity.ok(patient);
+        }
+        else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+    @GetMapping("/hello")
     public String helloWorld() {
         System.out.println("hello reached");
         return "Hello wordl";
--- backend/hospital_management/src/main/java/com/had/hospital_management/service/PatientService.java
+++ backend/hospital_management/src/main/java/com/had/hospital_management/service/PatientService.java
@@ -1,7 +1,28 @@
 package com.had.hospital_management.service;
 
 import com.had.hospital_management.model.Patient;
+import com.had.hospital_management.repository.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-public interface PatientService {
-    public Patient savePatient(Patient patient);
+import java.util.List;
+
+@Service
+public class PatientService {
+
+    @Autowired
+    private PatientRepository patientRepository;
+
+    public Patient save(Patient patient) {
+        System.out.println("savepatient reached");
+        System.out.println(patient);
+        return patientRepository.save(patient);
+    }
+    public List<Patient> findAll(){
+        return patientRepository.findAll();
+    }
+
+    public Patient findById(Long Id){
+        return patientRepository.findById(Id).orElse(null);
+    }
 }
