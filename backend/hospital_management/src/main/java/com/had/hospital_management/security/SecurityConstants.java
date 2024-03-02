3dc75bb7963f857c681c78ddb27ad8e12e2507b2
vatsal dhama <thevatsaldhama@gmail.com>
2024-03-02 18:59:42 +0530

    increased expiry

--- backend/hospital_management/src/main/java/com/had/hospital_management/security/SecurityConstants.java
+++ backend/hospital_management/src/main/java/com/had/hospital_management/security/SecurityConstants.java
@@ -1,6 +1,6 @@
 package com.had.hospital_management.security;
 
 public class SecurityConstants {
-    public static final long JWT_EXPIRATION = 7000;
+    public static final long JWT_EXPIRATION = 700000;
     public static final String JWT_SECRET = "secret";
 }
