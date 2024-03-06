39eb788893ea0c2149c65a0a779588f71ec95165
vatsal dhama <thevatsaldhama@gmail.com>
2024-03-06 23:15:50 +0530

    simplification is the step to evolution of humanity

--- backend/hospital_management/src/main/java/com/had/hospital_management/repository/RadiologistRepository.java
+++
@@ -1,9 +0,0 @@
-package com.had.hospital_management.repository;
-
-import com.had.hospital_management.model.Radiologist;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RadiologistRepository extends JpaRepository<Radiologist, Long> {
-}
