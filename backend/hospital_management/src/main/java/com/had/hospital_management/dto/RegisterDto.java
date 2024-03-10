2c3def01a53f796e5aa02364986c45ae9c9a2417
Arin0508 <arinawasthi05@gmail.com>
2024-03-10 18:36:55 +0530

    updated registerdto

--- backend/hospital_management/src/main/java/com/had/hospital_management/dto/RegisterDto.java
+++ backend/hospital_management/src/main/java/com/had/hospital_management/dto/RegisterDto.java
@@ -3,9 +3,16 @@ package com.had.hospital_management.dto;
 import com.had.hospital_management.model.Role;
 import lombok.Data;
 
+import java.util.Date;
+
 @Data
 public class RegisterDto {
-    private String username;
+    private String username;//email
     private String password;
+    private String first_name;
+    private String last_name;
+    private String gender;
+    private Date dob;
+    private String address;
     private Role role;
 }
