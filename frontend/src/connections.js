7d193d5758379b286b2df3c9c386ad390485aaa4
Arin0508 <arinawasthi05@gmail.com>
2024-03-30 00:21:32 +0530

    Register Integrated with frontend

--- frontend/src/connections.js
+++ frontend/src/connections.js
@@ -33,7 +33,56 @@ async function LoginCall(email, pass) {
     return [ret, jwt];
 }
 
+async function RegisterCall(
+uname,
+pass,
+fname,
+lname,
+genderr,
+dobb,
+addresss,
+roleid
+){
+    const url = "http://localhost:8081/api/auth/register";
+    let ret;
+    await fetch(url,
+    {
+        method: "POST",
+        body: JSON.stringify(
+        {
+        username: uname,
+        password: pass,
+        first_name: fname,
+        last_name: lname,
+        gender: genderr,
+//        dob: dobb
+        address : addresss,
+        role :
+        {
+            id : "1"
+        }
+        }),
+        headers: {
+        "Content-type": "application/json",
+        },
+    })
+    .then((data) => {
+    ret = data.ok;
+    return data.json();
+    })
+    .then((data) =>{
+    console.log(data);
+    })
+    .catch((err) => {
+    ret = false;
+    console.log(err.message);
+    });
+    console.log("RET: ", ret);
+    return ret;
+}
+
 export {
-    LoginCall
+    LoginCall,
+    RegisterCall
   };
   
