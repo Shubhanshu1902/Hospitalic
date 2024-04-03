51d7334dfbbef14d22d11dd2513da00abb61ae19
naitik-solanki <naitiksolanki2002@gmail.com>
2024-04-03 17:21:12 +0530

    Added functionality to view lost of reports for patient and doctor

--- backend/hospital_management/src/main/java/com/had/hospital_management/repository/ReportRepository.java
+++ backend/hospital_management/src/main/java/com/had/hospital_management/repository/ReportRepository.java
@@ -1,7 +1,22 @@
 package com.had.hospital_management.repository;
 import com.had.hospital_management.model.Report;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
 @Repository
 public interface ReportRepository extends JpaRepository<Report,Long>{
+    @Query(
+            nativeQuery = true,
+            value = "select * from report where patient_id = :patient_id"
+    )
+    List<Report> getReportByPateintId(@Param("patient_id") Long patient_id);
+    @Query(
+            nativeQuery = true,
+            value = "select * from report where lead_doctor_id = :doctor_id"
+    )
+    List<Report> getReportByDoctorId(@Param("doctor_id") Long doctor_id);
 }
