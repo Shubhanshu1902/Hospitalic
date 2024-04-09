fdbbe8ab48e38fbf9c4e64a3123c891d90c75843
Shubhanshu Agrawal <shubhanshu1902@gmail.com>
2024-04-09 15:42:46 +0530

    Update ReportService.java

--- backend/hospital_management/src/main/java/com/had/hospital_management/service/ReportService.java
+++ backend/hospital_management/src/main/java/com/had/hospital_management/service/ReportService.java
@@ -1,14 +1,38 @@
 package com.had.hospital_management.service;
 
 import com.had.hospital_management.model.Report;
+import com.had.hospital_management.model.Requests;
 import com.had.hospital_management.repository.ReportRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 @Service
 public class ReportService {
     @Autowired
     private ReportRepository reportRepository;
 
-    public Report saveReport(Report report){return reportRepository.save(report);}
+    public Report save(Report Requests)
+    {
+        return reportRepository.save(Requests);
+    }
+
+    public List<Report> findAll()
+    {
+        return reportRepository.findAll();
+    }
+
+    public Report getById(Long Id)
+    {
+        return  reportRepository.findById(Id).orElse(null);
+    }
+
+    @Transactional
+    public void deleteById(Long id) {
+        Report reports = reportRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Report with id " + id + " not found"));
+        reportRepository.delete(reports);
+    }
+
 }
