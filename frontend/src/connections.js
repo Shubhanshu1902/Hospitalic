c71b26bb0b7f8a6746931625edcc3a62464df734
Shubhanshu Agrawal <shubhanshu1902@gmail.com>
2024-04-11 16:55:56 +0530

    Update connections.js

--- frontend/src/connections.js
+++ frontend/src/connections.js
@@ -124,6 +124,33 @@ async function fetchChatEntities(reportId, token) {
     return chatEntities;
 }
 
+async function BookAppointment(
+    dt,
+    user1_id,
+    user2_id
+){
+    const url = "http://localhost:8081/appointment/save";
+    await fetch(url,
+    {
+        method: "POST",
+        body:JSON.stringify(
+        {
+            date:dt,
+            user1:
+            {
+                id: user1_id.toString()
+            },
+            user2:
+            {
+                id:user1_id.toString()
+            }
+        }),
+        headers: {
+        "Content-type": "application/json",
+        },
+    })
+}
+
 // retrieve JWT token
 export const retrieveJWT = () => {
     console.log("testing",cookie.get("JWTtoken"))
