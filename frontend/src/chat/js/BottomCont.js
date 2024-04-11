4f2c664b9df04a328a6831712e427cae34bdc4dc
Shubhanshu Agrawal <shubhanshu1902@gmail.com>
2024-04-11 16:52:00 +0530

    Update BottomCont.js

--- frontend/src/chat/js/BottomCont.js
+++ frontend/src/chat/js/BottomCont.js
@@ -4,7 +4,6 @@ import RequestModal from "./RequestModal";
 import InsightViewer, { useImage, useInteraction } from "@lunit/insight-viewer";
 import { useViewport } from "@lunit/insight-viewer/viewport";
 
-
 const BottomCont = (props) => {
     const[x, setButtonPopup] = useState(false);
     
