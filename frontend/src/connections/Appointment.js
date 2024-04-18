1f2d2a40736012b86c052025165911b6aeef61e5
naitik-solanki <naitiksolanki2002@gmail.com>
2024-04-18 22:53:02 +0530

    added GetAppByDoctorId

--- frontend/src/connections/Appointment.js
+++ frontend/src/connections/Appointment.js
@@ -174,7 +174,33 @@ export async function GetAppByLabId(
     catch(error){
         console.error(error);
     }
-
+}
+export async function GetAppByDoctorId(
+    id
+){
+    const url = "http://localhost:8081/appointment/get_appointment_by_doctor_id/" + id;
+    let ret;
+    let token = retrieveJWT();
+    try {
+        await fetch(url,
+            {
+                method: "GET",
+                headers: {
+                    "Content-type": "application.json",
+                    "Authorization": `Bearer ${token}`
+                }
+            })
+            .then(response => {
+                return (response.json());
+            })
+            .then(data => {
+                ret = data;
+            })
+        return ret;
+    }
+    catch(error){
+        console.error(error);
+    }
 }
 export async function AssignLab(
     appointment_id,lab_id
