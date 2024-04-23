a8edacd3a864a23731a25d641e4f2b2e70f52bae
vatsal007 <75558599+vatsal-dhama@users.noreply.github.com>
2024-04-23 14:25:24 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,20 @@ import { LoginScreen } from "./Login/js/LoginScreen";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Register } from "./Login/js/Register";
 import {Chatscreen} from "./chat/js/Chatscreen";
-import Dash from "./dashboard/js/Dash";
-import Appscreen from "./appointments/js/Appscreen";
-import { ReportAdd } from "./Lab/js/ReportAdd";
-import { LabDashboard } from "./Lab/js/LabDashboard";
-import { Tasks } from "./Lab/js/Tasks";
+import { Dashboard } from "./Patient/Dashboard/Dashboard";
+import { ReportScreen } from "./Patient/Reports/ReportScreen";
+import { RadioDashboard } from "./Radiologist/Dashboard/Dashboard";
+import { DocDashboard } from "./Doctor/Dashboard/Dashboard";
+import { DocAppointments } from "./Doctor/Appointment/Appointments";
+import { DocReportScreen } from "./Doctor/Report/ReportScreen";
+import { RadReportScreen } from "./Radiologist/Report/ReportScreen";
+import Appscreen from "./Patient/Appointment/Appscreen";
+import { LabDashboard } from "./Lab/Dashboard/LabDashboard";
+import { Tasks } from "./Lab/Tasks/Tasks";
+import { ReportsLab } from "./Lab/Reports/ReportsLab";
+import { AllReportLab } from "./Lab/Reports/AllReportLab";
+import PatHis from "./Patient/History/PatHis";
+
 
 function App() {
     
@@ -19,23 +28,31 @@ function App() {
                 <Route path='/register' element={<Register />} />
 
                 {/* DOCTOR */}
-                <Route path='/doctor/dashboard' element={<Dash />} />
+                <Route path='/doctor/dashboard' element={<DocDashboard />} />
                 <Route path='/doctor/chats/:reportId' element={<Chatscreen />} />
+                <Route path='/doctor/appointments' element={<DocAppointments />} />
+                <Route path='/doctor/reports' element={<DocReportScreen />} />
 
 
                 {/* PATIENT */}
-                <Route path='/patient/dashboard' element={<Dash />} />
+                <Route path='/patient/dashboard' element={<Dashboard />} />
                 <Route path='/patient/appointments' element={<Appscreen />} />
+                <Route path='/patient/reports' element={<ReportScreen />} />
+                <Route path='/patient/history' element={<PatHis />} />
+
+                
 
                 {/* LAB */}
-                <Route path='/addReport/:appointmentId' element={<ReportAdd />} />
                 <Route path='/lab/dashboard' element={<LabDashboard />} />
                 <Route path='/lab/tasks' element={<Tasks />} />
+                <Route path='/lab/reports' element={<ReportsLab />} />
+                <Route path='/lab/reports/:id' element={<AllReportLab />} />
 
                 {/* RADIOLOGIST */}
-                <Route path='/radiologist/dashboard' element={<Dash />} />
+                <Route path='/radiologist/dashboard' element={<RadioDashboard />} />
+                <Route path='/radiologist/dashboard' element={<RadReportScreen />} />
 
-
+                
 
             </Routes>
         </BrowserRouter>
