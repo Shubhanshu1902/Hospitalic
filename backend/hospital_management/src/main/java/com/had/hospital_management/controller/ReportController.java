2c1fd4c566127afb47f5b4ace486b98fb01cef57
naitik-solanki <naitiksolanki2002@gmail.com>
2024-05-06 21:11:29 +0530

    Added Preauth in ReportController

--- backend/hospital_management/src/main/java/com/had/hospital_management/controller/ReportController.java
+++ backend/hospital_management/src/main/java/com/had/hospital_management/controller/ReportController.java
@@ -3,8 +3,10 @@ package com.had.hospital_management.controller;
 import com.had.hospital_management.model.Report;
 import com.had.hospital_management.model.UserEntity;
 import com.had.hospital_management.service.ReportService;
+import com.had.hospital_management.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,7 +16,10 @@ import java.util.List;
 public class ReportController {
     @Autowired
     private ReportService reportService;
+    @Autowired
+    private UserService userService;
     @PostMapping("/save")
+    @PreAuthorize("#reports.getUser2().getUsername()== authentication.principal.username")
     public Report save(@RequestBody Report reports){
         // System.out.println("HERE IN REPORT SAVE");
         return reportService.save(reports);
@@ -27,34 +32,42 @@ public class ReportController {
     }
 
     @GetMapping("/get_report_by_patient_id/{id}")
+    @PreAuthorize("userService.findById(#id).getUsername()== authentication.principal.username")
     public List<Report> getReportByPateintId(@PathVariable("id") Long id){
         return reportService.getReportByPateintId(id);
     }
     @GetMapping("/get_report_by_doctor_id/{id}")
+    @PreAuthorize("userService.findById(#id).getUsername()== authentication.principal.username")
     public List<Report> getReportByDoctorId(@PathVariable("id") Long id){
         return reportService.getReportByDoctorId(id);
     }
     @GetMapping("/get_report_by_lab_id/{id}")
+    @PreAuthorize("userService.findById(#id).getUsername()== authentication.principal.username")
     public List<Report> getReportByLabId(@PathVariable("id") Long id){
         return reportService.getReportByLabId(id);
     }
     @GetMapping("/get_patient_by_doctor_id/{id}")
+    @PreAuthorize("userService.findById(#id).getUsername()== authentication.principal.username")
     public List<Long> getPatientByDoctorId(@PathVariable("id") Long id){
         return reportService.getPatientByDoctorId(id);
     }
     @GetMapping("/get_doctor_by_patient_id/{id}")
+    @PreAuthorize("userService.findById(#id).getUsername()== authentication.principal.username")
     public List<Long> getDoctorByPatientId(@PathVariable("id") Long id){
         return reportService.getDoctorByPatientId(id);
     }
     @GetMapping("/get_patient_by_lab_id/{id}")
+    @PreAuthorize("userService.findById(#id).getUsername()== authentication.principal.username")
     public List<Long> getPatientByLabId(@PathVariable("id") Long id){
         return reportService.getPatientByLabId(id);
     }
     @GetMapping("/get_report_by_doctor_and_patient_id/{doc_id}/{pat_id}")
+    @PreAuthorize("userService.findById(#doc_id).getUsername()== authentication.principal.username or userService.findById(#pat_id).getUsername()== authentication.principal.username")
     public List<Report> getReportByDoctorAndPatientId(@PathVariable("doc_id") Long doc_id,@PathVariable("pat_id") Long pat_id){
         return reportService.getReportByDoctorAndPatientId(doc_id,pat_id);
     }
     @GetMapping("/get_report_by_lab_and_patient_id/{lab_id}/{pat_id}")
+    @PreAuthorize("userService.findById(#lab_id).getUsername()== authentication.principal.username or userService.findById(#pat_id).getUsername()== authentication.principal.username")
     public List<Report> getReportByLabAndPatientId(@PathVariable("lab_id") Long lab_id,@PathVariable("pat_id") Long pat_id){
         return reportService.getReportByLabAndPatientId(lab_id,pat_id);
     }
@@ -68,8 +81,8 @@ public class ReportController {
             return ResponseEntity.notFound().build();
         }
     }
-
     @PostMapping("/add_comment/{id}")
+    @PreAuthorize("reportService.getById(#id).getUser1().getUsername() == authentication.principal.username")
     public void addComment(@PathVariable("id")Long id,@RequestBody String new_com){
         reportService.addComment(id,new_com);}
 }
